7b26b7510d98f1a6d93a5aa36c409c6a7341f0ad
Linus Bittner <2130@htl.rennweg.at>
2024-01-07 13:35:09 +0100

    Version 8 Darkmode Update

--- script.js
+++ script.js
@@ -34,31 +34,55 @@ darkmode.addEventListener("change", setDarkmode);
 let darkmodeActive = false;
 
 function setDarkmode() {
+  let fontWhitemode = "black";
+  let backgroundWhitemode="white";
+  let navBackgroundWhitemode = "white";
+
+  let fontDarkmode = "white";
+  let backgroundDarkmode="black";
+  let navBackgroundDarkmode = "grey";
+  
+
+  const darkmodeText = document.querySelectorAll(".darkmodeText");
+  const svg = document.querySelectorAll("svg");
+  const nav = document.querySelector("nav");
+  const settingsBackground = document.querySelector(".settings");
+
+  const randomNumBackground = document.querySelector(".randomNum");
+  const min = document.querySelector("#min");
+  const max = document.querySelector("#max");
+  const coinflipBackground = document.querySelector(".coinflip");
+  const diceBackground = document.querySelector(".dice");
+
   if (!darkmodeActive) {
-    document.querySelectorAll(".darkmodeText").forEach((link) => {
-      link.style.color = "white";
-    });
-    document.querySelectorAll("svg").forEach((link) => {
-      link.style.fill = "white";
-    });
-    document.querySelector(".randomNum").style.backgroundColor = "black";
-    document.querySelector("nav").style.backgroundColor = "rgb(105, 105, 105)";
-    document.querySelector(".settings").style.backgroundColor = "black";
-    document.querySelector("nav").style.boxShadow =
-      "3px 3px 5px rgba(255, 255, 255, 0.24)";
+    darkmodeText.forEach((link) => {link.style.color = fontDarkmode;});
+    svg.forEach((link) => {link.style.fill = fontDarkmode;});
+    nav.style.backgroundColor = navBackgroundDarkmode;
+    nav.style.boxShadow ="3px 3px 5px rgba(255, 255, 255, 0.24)";
+    settingsBackground.style.backgroundColor = backgroundDarkmode;
+    
+    randomNumBackground.style.backgroundColor = backgroundDarkmode;
+    min.style.borderBottom = "4px white solid";
+    max.style.borderBottom = "4px white solid";
+
+    coinflipBackground.style.backgroundColor = backgroundDarkmode;
+
+    diceBackground.style.backgroundColor = backgroundDarkmode;
     darkmodeActive = true;
+
   } else {
-    document.querySelectorAll(".darkmodeText").forEach((link) => {
-      link.style.color = "black";
-    });
-    document.querySelectorAll("svg").forEach((link) => {
-      link.style.fill = "black";
-    });
-    document.querySelector(".randomNum").style.backgroundColor = "white";
-    document.querySelector("nav").style.backgroundColor = "white";
-    document.querySelector(".settings").style.backgroundColor = "white";
-    document.querySelector("nav").style.boxShadow =
-      "3px 3px 5px rgba(0, 0, 0, 0.24)";
+
+    darkmodeText.forEach((link) => {link.style.color = fontWhitemode;});
+    svg.forEach((link) => {link.style.fill = fontWhitemode;});
+    randomNumBackground.style.backgroundColor = backgroundWhitemode;
+    nav.style.backgroundColor = navBackgroundWhitemode;
+    settingsBackground.style.backgroundColor = backgroundWhitemode;
+    nav.style.boxShadow ="3px 3px 5px rgba(0, 0, 0, 0.24)";
+
+
+    min.style.borderBottom = "4px black solid";
+    max.style.borderBottom = "4px black solid";
+
     darkmodeActive = false;
   }
 }
